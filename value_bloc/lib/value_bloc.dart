1e7e277c25727650930e38a0454df08522e3ae4e
SimoneBressan <simone@kuama.it>
2021-02-28 19:21:27 +0100

    release 0.5.0-dev.8

--- value_bloc/lib/value_bloc.dart
+++ value_bloc/lib/value_bloc.dart
@@ -2,10 +2,12 @@ library value_bloc;
 
 export 'package:bloc/bloc.dart';
 
+export 'src/auth/auth_cubit.dart';
+export 'src/cud/cud_cubit.dart';
 export 'src/iterable/IterableCubit.dart';
 export 'src/load/LoadCubit.dart';
 export 'src/navigation/NavigationCubit.dart';
 export 'src/object/ObjectCubit.dart';
-export 'src/utils.dart';
-export 'src/screen/DynamicCubit.dart';
 export 'src/screen/Closeable.dart';
+export 'src/screen/DynamicCubit.dart';
+export 'src/utils.dart';
